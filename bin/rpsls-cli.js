e9d15a7701b0a02f6843137510e02992d6f3f2fc
DidiQut <iamzhidi@gmail.com>
2023-02-21 13:06:04 -0500

    change the message

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,24 +1,21 @@
 #!/usr/bin/env node
-
 import minimist from "minimist";
 import { rpsls } from "../lib/rpsls.js";
 
-//help stuff
-const args = minimist(process.argv.slice(2));
+var args = minimist(process.argv.slice(2));
 
 const help_message = `Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-
   -h, --help        display this help message and exit
   -r, --rules       display the rules and exit
-
 Examples:
   node-rpsls        Return JSON with single player RPSLS result.
                     e.g. {"player":"rock"}
   node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}
+`;
 
-const rules = `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 - Scissors CUTS Paper
 - Paper COVERS Rock
 - Rock SMOOSHES Lizard
@@ -28,9 +25,10 @@ const rules = `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
 - Lizard EATS Paper
 - Paper DISPROVES Spock
 - Spock VAPORIZES Rock
-- Rock CRUSHES Scissors`;
+- Rock CRUSHES Scissors
+`;
 
-if (args.h || args.help) {
+if (args.h || args.rules) {
   console.log(help_message);
   process.exit(0);
 }
@@ -45,6 +43,5 @@ let shot = args._[0];
 try {
   console.log(JSON.stringify(rpsls(shot)));
 } catch (e) {
-  console.log(help_message);
   console.log(rules);
 }
